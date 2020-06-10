9bd1ed8890014809a781bc9a17a3bdf69572e869
Felix Zhang <18200116656@qq.com>
2020-06-10 16:12:29 +0800

    Daily commit

--- proxypool/storages/redis.py
+++ proxypool/storages/redis.py
@@ -28,7 +28,8 @@ class RedisClient(object):
         :param password: redis password
         """
         self.db = redis.StrictRedis(host=host, port=port, password=password, decode_responses=True, **kwargs)
-    
+
+    # add 方法向数据库添加代理并设置分数，默认的分数是 PROXY_SCORE_INIT 也就是 10，返回结果是添加的结果
     def add(self, proxy: Proxy, score=PROXY_SCORE_INIT) -> int:
         """
         add proxy and set it to init score
@@ -43,7 +44,9 @@ class RedisClient(object):
             if IS_REDIS_VERSION_2:
                 return self.db.zadd(REDIS_KEY, score, proxy.string())
             return self.db.zadd(REDIS_KEY, {proxy.string(): score})
-    
+
+    # random 方法是随机获取代理的方法，首先获取 100 分的代理，然后随机选择一个返回。
+    # 如果不存在 100 分的代理，则此方法按照排名来获取，选取前 100 名，然后随机选择一个返回，否则抛出异常。
     def random(self) -> Proxy:
         """
         get random proxy
@@ -62,7 +65,8 @@ class RedisClient(object):
             return convert_proxy_or_proxies(choice(proxies))
         # else raise error
         raise PoolEmptyException
-    
+
+    # decrease 方法是在代理检测无效的时候设置分数减 1 的方法，代理传入后，此方法将代理的分数减 1，如果分数达到最低值，那么代理就删除。
     def decrease(self, proxy: Proxy) -> int:
         """
         decrease score of proxy, if small than PROXY_SCORE_MIN, delete it
@@ -80,7 +84,8 @@ class RedisClient(object):
         else:
             logger.info(f'{proxy.string()} current score {score}, remove')
             return self.db.zrem(REDIS_KEY, proxy.string())
-    
+
+    # exists 方法可判断代理是否存在集合中。
     def exists(self, proxy: Proxy) -> bool:
         """
         if proxy exists
@@ -88,7 +93,8 @@ class RedisClient(object):
         :return: if exists, bool
         """
         return not self.db.zscore(REDIS_KEY, proxy.string()) is None
-    
+
+    # max 方法将代理的分数设置为 PROXY_SCORE_MAX，即 100，也就是当代理有效时的设置。
     def max(self, proxy: Proxy) -> int:
         """
         set proxy to max score
@@ -99,21 +105,24 @@ class RedisClient(object):
         if IS_REDIS_VERSION_2:
             return self.db.zadd(REDIS_KEY, PROXY_SCORE_MAX, proxy.string())
         return self.db.zadd(REDIS_KEY, {proxy.string(): PROXY_SCORE_MAX})
-    
+
+    # count 方法返回当前集合的元素个数
     def count(self) -> int:
         """
         get count of proxies
         :return: count, int
         """
         return self.db.zcard(REDIS_KEY)
-    
+
+    # all 方法返回所有的代理列表，供检测使用
     def all(self) -> List[Proxy]:
         """
         get all proxies
         :return: list of proxies
         """
         return convert_proxy_or_proxies(self.db.zrangebyscore(REDIS_KEY, PROXY_SCORE_MIN, PROXY_SCORE_MAX))
-    
+
+    # batch 方法返回一批代理
     def batch(self, start, end) -> List[Proxy]:
         """
         get batch of proxies
